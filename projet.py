52daefac84aaca55e431afbdd5c9d5b55a73034e
LYCRIES <payonlycries@gmail.com>
2025-07-26 04:56:33 +0100

    hellllo

---
+++ projet.py
@@ -0,0 +1,25 @@
+import requests
+
+endpoint = "https://jsearch.p.rapidapi.com/search"
+
+api_key = "57cc8d1375msh68815a3c5ed88fbp192a56jsn1ac0d6b85f85"
+api_host = "jsearch.p.rapidapi.com"
+hearders{
+    "x-rapidapi-key" = api_key,
+    "x-rapidapi-host" = "jsearch.p.rapidapi.com"
+} 
+parametre = {
+    "query" : recherche_d_emploi,
+    "page" = 1,
+    "contry" = contry
+}
+response = requests.get(url,hearders=hearders,params=parametre)
+#dictionnaire des pays
+countries = {
+    "1" : "us",
+    "2" : "ca",
+    "3" ; "ae"
+}
+#choix du pays
+ def choisir_pays():
+    print("choisir un pays :")
